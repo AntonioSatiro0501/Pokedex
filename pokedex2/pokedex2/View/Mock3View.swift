8c6f48c51ea33d9c1bc4d0d77a854bc347da7815
Gustavo Frainer <guga.frainer1@gmail.com>
2025-10-24 15:16:32 -0300

    Implement blinking effect for start text
    Added a blinking effect to the 'Toque para iniciar' text using a timer.

--- pokedex2/pokedex2/View/Mock3View.swift
+++ pokedex2/pokedex2/View/Mock3View.swift
@@ -1,16 +1,35 @@
 import SwiftUI
 
 struct Mock3View: View {
+    @State private var isVisible = true
+    
     var body: some View {
         NavigationStack {
             NavigationLink(destination: Mock2View()) {
                 VStack {
-                    Spacer(minLength: 500)
-                    
-                    Text("Toque para iniciar")
-                        .font(.title)
-                        .foregroundColor(.gray)
-                    Spacer()
+                    ZStack {
+                        Image("TelaFundo")
+                            .blur(radius: 6)
+                        VStack {
+                            Spacer(minLength: 300)
+                            Image("PokemonLogo2")
+                                .resizable()
+                                .frame(width: 650, height: 650)
+                            Spacer()
+                            Text("Toque para iniciar")
+                                .font(.title)
+                                .opacity(isVisible ? 1.0 : 0.2)
+                                .foregroundColor(.black)
+                            Spacer(minLength: 500)
+                        }
+                    }
+                }
+                .task {
+                    Timer.scheduledTimer(withTimeInterval: 0.75, repeats: true) { _ in
+                        withAnimation(.easeInOut(duration: 0.8)) {
+                            isVisible.toggle()
+                        }
+                    }
                 }
             }
         }
